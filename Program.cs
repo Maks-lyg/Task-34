d88c663b0fa3e3ec89866972ab4051c89bc1b28a
Maks <maks-lug333@yandex.ru>
2023-03-01 20:09:05 +0900

    ne rabotaet

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int[] Array(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0;i < size; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+    return array;
+}
+int Hou(int[] a)
+{
+    
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i] % 2 ==0)
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите разрядность массива");
+int size = int.Parse(Console.ReadLine());
+int[] mass = Array(size);
+int x = Hou(a);
+Console.Write($" {String.Join(", ", mass)} ");
+Console.Write($"Кол-во ч чисел = {x}");
