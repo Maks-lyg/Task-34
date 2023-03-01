bc431485c5eba7b0df72f34fecc445e3815ed0f9
Maks <maks-lug333@yandex.ru>
2023-03-01 21:04:21 +0900

    ok

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿int[] Array(int size)
+﻿int Hou(int[] ar)
+{
+    
+    int count = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (ar[i] % 2 == 0) // я тут что только не менял, туплю, застрял на месте
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
+int[] Array(int size)
 {
     int[] array = new int[size];
     for (int i = 0;i < size; i++)
@@ -7,24 +20,13 @@
     }
     return array;
 }
-int Hou(int[] a)
-{
-    
-    int count = 0;
-    for (int i = 0; i < a.Length; i++)
-    {
-        if (a[i] % 2 ==0)
-        {
-            count +=1;
-        }
-    }
-    return count;
-}
+
 
 Console.Clear();
 Console.WriteLine("Введите разрядность массива");
 int size = int.Parse(Console.ReadLine());
 int[] mass = Array(size);
-int x = Hou(a);
+int s = Hou(ar);//не видит переменную, менял и на переменные. Вывод именно count делал(ну а вдруг)))) 
+Console.Write($"чч - {s} ");
 Console.Write($" {String.Join(", ", mass)} ");
-Console.Write($"Кол-во ч чисел = {x}");
+Console.Write("Всё?");
